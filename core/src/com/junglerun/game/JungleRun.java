f8509a5f1b39dd8c0f0f4f572ca87940996fe126
Anisio Munda <anisiomunda@hotmail.com>
2019-09-07 16:47:57 +0100

    Adicionado movimento ao Sprite do Ninja

--- core/src/com/junglerun/game/JungleRun.java
+++ core/src/com/junglerun/game/JungleRun.java
@@ -17,22 +17,44 @@ public class JungleRun extends ApplicationAdapter {
     private int larguraPadrãoX;
     private int alturaPadrãoY;
 
+    //variavel para trabalhar com o indice do vector para alterar sprites
+	private float indiceSprite;
+
 	@Override
 	public void create () {
 
 	    batch = new SpriteBatch();
 	    fundo = new Texture("fundo1.png");
 
+	    //Instância do vector do sprite
+		ninja = new Texture[10];
+
+	    //Preenchendo o vector com sprite personagem ao vector
+	    for (int i=0;i<10;i++){
+	    	ninja[i] = new Texture("ninja"+(i+1)+".png");
+		}
+
 	    larguraPadrãoX = Gdx.graphics.getWidth();
 	    alturaPadrãoY = Gdx.graphics.getHeight();
+
+	    //Inicializar o indice do Sprite
+		indiceSprite = 0;
 	}
 
 	@Override
 	public void render () {
 
+		indiceSprite += Gdx.graphics.getDeltaTime() * 10;
+
+		//se o indice do Sprite for maior que 9 zerar novamente o indice do Sprite
+		if (indiceSprite>9){
+			indiceSprite = 0;
+		}
+		Gdx.app.log("Valor: ",""+indiceSprite);
         batch.begin();
 
         batch.draw(fundo,0,0,larguraPadrãoX,alturaPadrãoY);
+        batch.draw(ninja[(int)indiceSprite],400,300);
 
         batch.end();
 	}
