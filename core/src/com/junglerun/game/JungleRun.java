c6181aad8de836c0f301575e1dcd2d4368447b61
Anisio Munda <anisiomunda@hotmail.com>
2019-09-07 13:50:28 +0100

    Adicionado o fundo do jogo

--- core/src/com/junglerun/game/JungleRun.java
+++ core/src/com/junglerun/game/JungleRun.java
@@ -2,32 +2,44 @@ package com.junglerun.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class JungleRun extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+
+    //Texturas e elementos para o jogo
+    private SpriteBatch batch;
+    private Texture fundo;
+    private Texture [] ninja;
+
+
+    //dimensões de largura e altura padrão
+    private int larguraPadrãoX;
+    private int alturaPadrãoY;
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
+	    batch = new SpriteBatch();
+	    fundo = new Texture("fundo1.png");
+
+	    larguraPadrãoX = Gdx.graphics.getWidth();
+	    alturaPadrãoY = Gdx.graphics.getHeight();
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+
+        batch.begin();
+
+        batch.draw(fundo,0,0,larguraPadrãoX,alturaPadrãoY);
+
+        batch.end();
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+
+        batch.dispose();
 	}
 }
