746e606332b6a36c8666f7b0fdb98571260436db
Leotom Victoriano <leotompatricio@hotmail.com>
2019-09-09 11:27:29 +0100

    Mecânica de Saldo adicionado (com necessidade de refactorar).

--- core/src/com/junglerun/game/JungleRun.java
+++ core/src/com/junglerun/game/JungleRun.java
@@ -21,42 +21,67 @@ public class JungleRun extends ApplicationAdapter {
     //variavel para trabalhar com o indice do vector para alterar sprites
 	private float indiceSprite;
 
+	//
+	private float deltaTime;
+	//propriedades de salto
+	private boolean salto;
+	private int alturaMaximaDeSalto;
+	private int alturaMinimaDeQueda;
+    private int posNinjaY;
 	@Override
 	public void create () {
 
 	    batch = new SpriteBatch();
 	    fundo = new Texture("fundo1.png");
-
+	    salto=false;
 	    //Instância do vector do sprite
 		ninja = new Texture[10];
 	    //Preenchendo o vector com sprite personagem ao vector
 	    for (int i=0;i<10;i++){
 	    	ninja[i] = new Texture("ninja"+(i+1)+".png");
 		}
-
 	    larguraPadraoX = Gdx.graphics.getWidth();
 	    alturaPadraoY = Gdx.graphics.getHeight();
 	    //Obter o tamanho do fundo
 		alturaFundo = fundo.getHeight();
 		larguraFundo= fundo.getWidth();
+        //setting a posicao do ninja
+        posNinjaY=(int)(alturaFundo*0.30);
 	    //Inicializar o indice do Sprite
 		indiceSprite = 0;
+		//propriedades de salto
+		alturaMinimaDeQueda=posNinjaY;
+		alturaMaximaDeSalto=posNinjaY+140;
 	}
 
 	@Override
 	public void render () {
-
 		indiceSprite += Gdx.graphics.getDeltaTime() * 10;
+
+		//controle de queda
+		if(posNinjaY>alturaMinimaDeQueda){
+			indiceSprite=7;
+			posNinjaY-=Gdx.graphics.getDeltaTime()*200;
+			salto=false;
+		}
 
 		//se o indice do Sprite for maior que 9 zerar novamente o indice do Sprite
 		if (indiceSprite>9){
 			indiceSprite = 0;
 		}
+		if(Gdx.input.justTouched()){
+			if(!salto){
+				if(posNinjaY<=alturaMaximaDeSalto){
+				posNinjaY+=138+deltaTime;
+				salto=true;
+				}
+			}
+		}
 		Gdx.app.log("Valor: ",""+indiceSprite);
         batch.begin();
 
         batch.draw(fundo,0,0,larguraPadraoX,alturaPadraoY);
-        batch.draw(ninja[(int)indiceSprite],10,(int)(alturaFundo*0.30));
+        batch.draw(ninja[(int)indiceSprite],50,posNinjaY);
 
         batch.end();
 	}
